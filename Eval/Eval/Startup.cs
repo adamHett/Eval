01ab3dfa372cbbde4905e73d600f4f35326e5272
adam hett <hett.adam@gmail.com>
2017-05-25 18:40:58 -0400

    start

---
+++ Eval/Eval/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eval.Startup))]
+namespace Eval
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
